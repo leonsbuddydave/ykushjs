291fb3fcf3cde81cec087c546762419829103729
Jussi Vatjus-Anttila <jussiva@gmail.com>
2020-05-27 09:50:00 +0300

    udev fix installation script

--- bin/linux/install_udev.sh
+++ bin/linux/install_udev.sh
@@ -15,8 +15,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-
-cp 99-ykush.rules /etc/udev/rules.d/99-ykush.rules
+wget https://raw.githubusercontent.com/OpenTMI/ykushjs/master/bin/linux/99-ykush.rules -O /etc/udev/rules.d/99-ykush.rules
 chmod 644 /etc/udev/rules.d/99-ykush.rules
 usermod -a -G dialout $USER
 udevadm control --reload-rules && udevadm trigger
